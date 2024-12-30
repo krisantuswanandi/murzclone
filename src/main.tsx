9e18a8dd081bee71366b1b942926f641723f1810
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-12-30 13:41:45 +0700

    shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen.ts'
+import './index.css'
 
 const router = createRouter({ routeTree })
 
--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,8 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/main')({
   component: () => {
-    return <div>Main Page</div>
+    return <div>
+      <div>
+        Main Page
+      </div>
+      <div>
+        <Button>
+          Testing
+        </Button>
+      </div>
+    </div>
   },
 })
 
