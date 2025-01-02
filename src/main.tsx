1ad792a0e7adfe99fbec4dc4d15be61ed536cf90
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2025-01-02 19:24:42 +0700

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { setupFirebase } from "@/configs/firebase.ts";
 import { createQueryClient } from "@/configs/query.ts";
 import { routeTree } from "./routeTree.gen.ts";
@@ -20,6 +21,7 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
